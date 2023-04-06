4e49b0db0696012b76c8feba6a6087662e03b68e
edubew <edubew98@gmail.com>
2023-04-06 13:18:35 +0300

    Add an array of books

--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -1,8 +1,29 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const ADD_BOOK = 'bookstore/books/addBook';
 const REMOVE_BOOK = 'bookstore/books/removeBook';
 
 const initialState = {
-  books: [],
+  books: [
+    {
+      id: uuidv4(),
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      id: uuidv4(),
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      id: uuidv4(),
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
 };
 
 // Add Reducers
