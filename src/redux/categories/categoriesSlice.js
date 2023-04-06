fa1862f17805f97d07462adc879216fa5b7a6dea
edubew <edubew98@gmail.com>
2023-04-06 09:53:40 +0300

    Fix eslint errors

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,18 +1,18 @@
 const CHECK_STATUS = 'bookstore/categories/checkStatus';
 
 const initialState = {
-    categories: [],
+  categories: [],
 };
 
-export default function categoriesReducer(state = initialState, action){
-    switch (action.type) {
-        case CHECK_STATUS:
-            return 'Under Construction';
-        default:
-            return state;
-    }
+export default function categoriesReducer(state = initialState, action) {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return 'Under Construction';
+    default:
+      return state;
+  }
 }
 
 export const checkStatus = () => ({
-    type: CHECK_STATUS,
+  type: CHECK_STATUS,
 });
