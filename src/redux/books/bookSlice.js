e50d52170bce73326647d59065565debdd6f72b0
edubew <edubew98@gmail.com>
2023-04-07 03:08:56 +0300

    Updates the bookSlice reducers

--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -1,46 +1,32 @@
 import { v4 as uuidv4 } from 'uuid';
 
+const BOOKS_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/fTVe6hSKCBLfTOyQqdDN/books';
+
 const ADD_BOOK = 'bookstore/books/addBook';
 const REMOVE_BOOK = 'bookstore/books/removeBook';
+const FETCH_BOOKS = 'bookstore/books/fetchBooks';
 
-const initialState = {
-  books: [
-    {
-      id: uuidv4(),
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      id: uuidv4(),
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      id: uuidv4(),
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
-};
+const initialState = [];
 
 // Add Reducers
 export default function booksReducer(state = initialState, action) {
   switch (action.type) {
-    case ADD_BOOK: {
-      const newBook = [...state.books, action.payload];
-      return {
-        ...state.books,
-        books: newBook,
-      };
-    }
-    case REMOVE_BOOK:
-      return {
-        ...state,
-        books: state.books.filter((book) => book.id !== action.payload.id),
-      };
+    case `${ADD_BOOK}/fulfilled`:
+      return action.payload;
+
+    case `${FETCH_BOOKS}/fulfilled`:
+      return Object.keys(action.payload).map((key) => {
+        const { title, author, category } = action.payload[key][0];
+        return {
+          item_id: key,
+          title,
+          author,
+          category,
+        };
+      });
+
+    case `${REMOVE_BOOK}/fulfilled`:
+      return state.filter((book) => book.id !== action.payload);
     default:
       return state;
   }
